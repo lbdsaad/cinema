7d9f59925aea84e1f14dd428f2405896373d68af
Saad Labied <saad.labied.95@gmail.com>
2021-01-21 02:50:56 +0100

    start

--- cinema/src/main/java/org/sid/cinema/entities/TicketProjection.java
+++ cinema/src/main/java/org/sid/cinema/entities/TicketProjection.java
@@ -1,4 +1,7 @@
 package org.sid.cinema.entities;
 
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name="ticketsProj", types = Ticket.class)
 public interface TicketProjection {
 }
